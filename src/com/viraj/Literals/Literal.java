89985dddcb1fe909ae868eb7923eb24f1d43a0d4
Viraj Pai <coder.viraj@gmail.com>
2018-01-18 23:24:13 +0530

    Literal - My first commit

---
+++ src/com/viraj/Literals/Literal.java
@@ -0,0 +1,25 @@
+package com.viraj.Literals;
+
+public class Literal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int positiveNumber = 5;
+		byte pos1 = 5;
+		short pos2 = 5;
+		long pos3 = 5;
+		
+		System.out.println(positiveNumber + pos1 + pos2 + pos3);
+		
+		long bigNumber = 23_55_0000;
+		System.out.println(bigNumber);
+		
+		char a = 'a';
+		char b = 'b';
+		char c = 'c';
+		
+
+	}
+
+}
